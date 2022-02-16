769e07efdb210e4c0e4a387622f39cb749dbcc70
heqian <182359041@qq.com>
2022-02-17 01:32:02 +0800

    import

--- pkg/build/build.go
+++ pkg/build/build.go
@@ -3,11 +3,13 @@ package build
 import (
 	"hbuf/pkg/dart"
 	"hbuf/pkg/parser"
+	"hbuf/pkg/scanner"
 	"hbuf/pkg/token"
+	"io/fs"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 )
 import "hbuf/pkg/golang"
 
@@ -22,20 +24,20 @@ func CheckType(typ string) bool {
 }
 
 func Build(out string, in string, typ string) error {
-	stat, err := os.Stat(in)
-	if err != nil {
-		return err
-	}
-	reg, err := regexp.Compile(stat.Name())
+	in = filepath.Clean(in)
+	path := filepath.Dir(in)
+	name := in[len(path)+1:]
+	reg, err := regexp.Compile(strings.ReplaceAll(name, "*", "(.*)"))
 	if err != nil {
 		return err
 	}
 
 	fset := token.NewFileSet() // positions are relative to fset
-	err = buildDir(fset, filepath.Dir(in), reg)
+	err = buildDir(fset, path, reg)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -54,8 +56,40 @@ func buildDir(fset *token.FileSet, path string, reg *regexp.Regexp) error {
 		if !reg.MatchString(item.Name()) {
 			continue
 		}
-		_, err := parser.ParseFile(fset, filepath.Join(path, item.Name()), nil, parser.AllErrors|parser.ParseComments)
+		err := parseFile(fset, path, item.Name())
 		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func parseFile(fset *token.FileSet, path string, name string) error {
+	file := filepath.Join(path, name)
+	if nil != fset.GetFileByName(file) {
+		return nil
+	}
+	f, err := parser.ParseFile(fset, file, nil, parser.AllErrors|parser.ParseComments)
+	if err != nil {
+		return err
+	}
+	for _, spec := range f.Imports {
+		imp := spec.Path.Value
+		imp = imp[1 : len(imp)-1]
+		if !("/" == imp || 0 != len(filepath.VolumeName(imp))) {
+			imp = filepath.Join(path, imp)
+		}
+		path := filepath.Dir(imp)
+		name := imp[len(path)+1:]
+		err := parseFile(fset, path, name)
+		if err != nil {
+			switch err.(type) {
+			case *fs.PathError:
+				return scanner.Error{
+					Pos: fset.Position(spec.Pos()),
+					Msg: "Not import: " + spec.Path.Value,
+				}
+			}
 			return err
 		}
 	}

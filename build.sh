644d22bee7e02571ba655639190657dcfcacb236
heqian <182359041@qq.com>
2023-02-11 18:01:57 +0800

    修改BUG

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ echo "开始打包 hbuf darwin..........."
 go build -o ./bin/hbuf_darwin ./pkg/compile/main.go
 echo "打包 hbuf darwin版本 成功"
 cp ./bin/hbuf_darwin /Users/heqian/dev/hbuf/hbuf_frame/hbuf_darwin
+cp ./bin/hbuf_darwin /Users/heqian/dev/apk_rebuild/hbuf_darwin
 
 export CGO_ENABdeLED=0
 export GOOS=windows

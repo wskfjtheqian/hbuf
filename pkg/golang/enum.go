9e76ee84a70a903e3f9227537a0f9befdbf6d311
heqian <182359041@qq.com>
2023-04-27 22:31:25 +0800

    修改Go枚举方式

--- pkg/golang/enum.go
+++ pkg/golang/enum.go
@@ -3,6 +3,7 @@ package golang
 import (
 	"hbuf/pkg/ast"
 	"hbuf/pkg/build"
+	"strings"
 )
 
 func printEnumCode(dst *build.Writer, typ *ast.EnumType) {
@@ -10,38 +11,50 @@ func printEnumCode(dst *build.Writer, typ *ast.EnumType) {
 	if nil != typ.Doc && 0 < len(typ.Doc.Text()) {
 		dst.Code("//" + name + " " + typ.Doc.Text())
 	}
+	maxLen := 0
 	dst.Code("type " + name + " int\n\n")
 	for _, item := range typ.Items {
-		itemName := name + build.StringToHumpName(item.Name.Name)
-		if nil != item.Doc && 0 < len(item.Doc.Text()) {
-			dst.Code("//" + itemName + " " + item.Doc.Text())
+		itemName := build.StringToHumpName(item.Name.Name)
+		l := len(itemName)
+		if l > maxLen {
+			maxLen = l
 		}
-		dst.Code("const " + itemName + " " + name + " = " + item.Id.Value + "\n\n")
+		if nil != item.Doc && 0 < len(item.Doc.Text()) {
+			dst.Code("//" + name + itemName + " " + item.Doc.Text())
+		}
+		dst.Code("const " + name + itemName + " " + name + " = " + item.Id.Value + "\n\n")
 	}
-
-	dst.Code("func (e " + name + ") ToName() string {\n")
-	dst.Code("\tswitch e {\n")
-	for _, item := range typ.Items {
-		dst.Code("\tcase " + name + build.StringToHumpName(item.Name.Name) + ":\n")
-		dst.Code("\t\treturn \"" + build.StringToHumpName(item.Name.Name) + "\"\n")
-	}
-	dst.Code("\t}\n")
-	dst.Code("\treturn \"\"\n")
-	dst.Code("}\n\n")
-
-	dst.Code("func (e " + name + ") OfName(name string) (" + name + ", error) {\n")
-	dst.Code("\tswitch name {\n")
-	for _, item := range typ.Items {
-		dst.Code("\tcase \"" + build.StringToHumpName(item.Name.Name) + "\":\n")
-		dst.Code("\t\treturn " + name + build.StringToHumpName(item.Name.Name) + ", nil\n")
-	}
-	dst.Code("\t}\n")
-	dst.Import("errors", "")
-	dst.Code("\treturn 0, errors.New(name + \" not to " + name + "\")\n")
-	dst.Code("}\n\n")
 
 	dst.Code("func (e " + name + ") Pointer() *" + name + " {\n")
 	dst.Code("	pointer := e\n")
 	dst.Code("	return &pointer\n")
 	dst.Code("}\n\n")
+
+	space := strings.Builder{}
+	for i := 0; i < maxLen; i++ {
+		space.WriteString(" ")
+	}
+	spaceText := space.String()
+	dst.Code("var " + build.StringToFirstLower(typ.Name.Name) + "Map = map[" + name + "]string{\n")
+	for _, item := range typ.Items {
+		enumItem := build.StringToHumpName(item.Name.Name)
+		dst.Code("\t" + name + enumItem + ": " + spaceText[:maxLen-len(enumItem)] + "\"" + build.StringToHumpName(item.Name.Name) + "\",\n")
+	}
+	dst.Code("}\n\n")
+
+	dst.Code("func (e " + name + ") ToName() string {\n")
+	dst.Code("\treturn " + build.StringToFirstLower(typ.Name.Name) + "Map[e]\n")
+	dst.Code("}\n\n")
+
+	dst.Code("var " + build.StringToFirstLower(typ.Name.Name) + "Values = map[string]" + name + "{\n")
+	for _, item := range typ.Items {
+		enumItem := build.StringToHumpName(item.Name.Name)
+		dst.Code("\t\"" + enumItem + "\": " + spaceText[:maxLen-len(enumItem)] + name + build.StringToHumpName(item.Name.Name) + ",\n")
+	}
+	dst.Code("}\n\n")
+
+	dst.Code("func " + name + "Values() map[string]" + name + "{\n")
+	dst.Code("\treturn " + build.StringToFirstLower(typ.Name.Name) + "Values\n")
+	dst.Code("}\n\n")
+
 }

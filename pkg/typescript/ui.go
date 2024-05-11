0e0894ecf022d8b9cab08501a536c5f3a0baeb94
何乾 <182359041@qq.com>
2024-05-11 17:06:38 +0800

    完善VUE datepackager

--- pkg/typescript/ui.go
+++ pkg/typescript/ui.go
@@ -435,15 +435,15 @@ func (b *Builder) printForm(dst *build.Writer, typ *ast.DataType, u *ui) {
 			if isArray {
 				dst.Import("hbuf_ts", "* as h")
 				if "datetime" == form.form {
-					dst.Code("h.convertArray($event, (e) => _ctx.$timeToLocal(e))")
+					dst.Code("h.convertArray($event, (e) => _ctx.$timeToUtc(e))")
 				} else if "month" == form.form {
-					dst.Code("h.convertArray([$event![0], new Date($event![1].getFullYear(), $event[1]!.getMonth() + 1, 0, 23, 59, 59, 999)], (e) => _ctx.$timeToLocal(e))")
+					dst.Code("h.convertArray([$event![0], new Date($event![1].getFullYear(), $event[1]!.getMonth() + 1, 0, 23, 59, 59, 999)], (e) => _ctx.$timeToUtc(e))")
 				} else if "dates" == form.form {
-					dst.Code("h.convertArray($event, (e) => _ctx.$timeToLocal(e))")
+					dst.Code("h.convertArray($event, (e) => _ctx.$timeToUtc(e))")
 				} else if "year" == form.form {
-					dst.Code("h.convertArray($event, (e) => _ctx.$timeToLocal(e))")
+					dst.Code("h.convertArray($event, (e) => _ctx.$timeToUtc(e))")
 				} else {
-					dst.Code("h.convertArray([$event![0], new Date($event![1]?.setHours(23,59,59,999))], (e) => _ctx.$timeToLocal(e))")
+					dst.Code("h.convertArray([$event![0], new Date($event![1]?.setHours(23,59,59,999))], (e) => _ctx.$timeToUtc(e))")
 				}
 			} else {
 				if "datetime" == form.form {

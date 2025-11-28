9678aa7bce7a706520c77d7cf4a40fa30b8a546c
call <call@qq.com>
2025-11-28 09:26:38 +0800

    fix(core): 修正字符串拼接问题

--- pkg/golang/mq.go
+++ pkg/golang/mq.go
@@ -39,7 +39,7 @@ func (b *Builder) printPublishCode(dst *build.Writer, data *ast.DataType, tag *a
 	name := build.StringToHumpName(data.Name.Name)
 	subject := strings.ReplaceAll(build.StringToUnderlineName(data.Name.Name), "_", ".")
 	dst.Tab(0).Code("func (g ").Code(name).Code(") Publish(ctx context.Context, subject string) error {\n")
-	dst.Tab(1).Code("return hmq.Publish(ctx, \"").Code(subject).Code(".\" + subject, &g)\n")
+	dst.Tab(1).Code("return hmq.Publish(ctx, \"").Code(subject).Code(".\"+subject, &g)\n")
 	dst.Tab(0).Code("}\n\n")
 	return nil
 }
@@ -48,7 +48,7 @@ func (b *Builder) printSubscribeCode(dst *build.Writer, data *ast.DataType, tag 
 	name := build.StringToHumpName(data.Name.Name)
 	subject := strings.ReplaceAll(build.StringToUnderlineName(data.Name.Name), "_", ".")
 	dst.Tab(0).Code("func (g ").Code(name).Code(") Subscribe(ctx context.Context, subject string, handler func(ctx context.Context, subject string, msg *").Code(name).Code(") error) (*nats.Subscription, error) {\n")
-	dst.Tab(1).Code("return hmq.Subscribe(ctx, \"").Code(subject).Code(".\" + subject, handler)\n")
+	dst.Tab(1).Code("return hmq.Subscribe(ctx, \"").Code(subject).Code(".\"+subject, handler)\n")
 	dst.Tab(0).Code("}\n\n")
 	return nil
 }
@@ -58,7 +58,7 @@ func (b *Builder) printJetStreamPublishCode(dst *build.Writer, data *ast.DataTyp
 	stream := build.StringToUnderlineName(data.Name.Name)
 	subject := strings.ReplaceAll(build.StringToUnderlineName(data.Name.Name), "_", ".")
 	dst.Tab(0).Code("func (g ").Code(name).Code(") JsPublish(ctx context.Context, subject string, options ...hmq.PublishOption) (*jetstream.PubAck, error) {\n")
-	dst.Tab(1).Code("return hmq.JetStreamPublish(ctx, \"stream_").Code(stream).Code("\", \"").Code(subject).Code(".\" + subject, &g, options...)\n")
+	dst.Tab(1).Code("return hmq.JetStreamPublish(ctx, \"stream_").Code(stream).Code("\", \"").Code(subject).Code(".\"+subject, &g, options...)\n")
 	dst.Tab(0).Code("}\n\n")
 	return nil
 }
@@ -68,7 +68,7 @@ func (b *Builder) printJetStreamSubscribeCode(dst *build.Writer, data *ast.DataT
 	stream := build.StringToUnderlineName(data.Name.Name)
 	subject := strings.ReplaceAll(build.StringToUnderlineName(data.Name.Name), "_", ".")
 	dst.Tab(0).Code("func (g ").Code(name).Code(") JsSubscribe(ctx context.Context, subject string, durable string, handler func(ctx context.Context, subject, msgId string, msg *").Code(name).Code(") error, options ...hmq.SubscribeOption) error {\n")
-	dst.Tab(1).Code("return hmq.JetStreamSubscribe(ctx, \"stream_").Code(stream).Code("\", \"").Code(subject).Code(".\" + subject, durable, handler, options...)\n")
+	dst.Tab(1).Code("return hmq.JetStreamSubscribe(ctx, \"stream_").Code(stream).Code("\", \"").Code(subject).Code(".\"+subject, durable, handler, options...)\n")
 	dst.Tab(0).Code("}\n\n")
 	return nil
 }
